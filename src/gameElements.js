8d73d12669622a2904232c2e9b27e2256d69e8d7
s0ur4bhkumar <soul65917@gmail.com>
2026-02-24 14:56:43 +0530

    created test for placement of ship with length 1

--- src/gameElements.js
+++ src/gameElements.js
@@ -1,38 +1,107 @@
-const ship = (length = 1, hitCount = 0) =>{
+const ship = (length = 1, hitCount = 0) => {
   return {
-    length: length <= 5 ? length: null,
+    length: length <= 4 && length !== 0 ? length : null,
     hitCount: hitCount,
     hit() {
-      this.hitCount++
+      this.hitCount++;
     },
     isSunk() {
       if (this.hitCount === this.length) {
-        return true
+        return true;
+      } else {
+        return false;
       }
-      else {
-        return false
-      }
-    }
-  }
-}
+    },
+  };
+};
 
 const gameBoard = () => {
-  let board = []
+  let board = [];
   for (let i = 0; i < 10; i++) {
-    let row = []
-    for (let j = 0; j < 10; j++){
-      row.push(j)
+    let row = [];
+    for (let j = 0; j < 10; j++) {
+      row.push(j);
     }
-    board.push(row)
+    board.push(row);
+  }
+  function isTopRow(x, y) {
+    return y === 0;
+  }
+  function isBottomRow(x, y) {
+    return y === 9;
+  }
+  function isLeftMostColoumn(x, y) {
+    return x === 0;
+  }
+  function isRightMostColumn(x, y) {
+    return x === 9;
   }
   return {
     board: board,
-    placeShip(x, y, Ship = ship()) {
-      if (Ship.length === 1) {
-        board[x][y] = Ship
+    placeSingle(x, y, Ship = ship()) {
+      //function to place ships with length 1 or unspecified length
+      if (Number.isInteger(board[x][y])) {
+        if (isTopRow(x, y) && isLeftMostColoumn(x, y)) {
+          board[x][y] = Ship; // [0][0]
+          board[x + 1][y] = " "; //for [1,0]
+          board[x][y + 1] = " "; // for [0][1]
+          board[x + 1][y + 1] = " "; // for [1][1]
+        } else if (isTopRow(x, y) && isRightMostColumn(x, y)) {
+          board[x][y] = Ship; //[9][0]
+          board[x - 1][y] = " "; //for [8][0]
+          board[x - 1][y + 1] = " "; // for [8][1]
+          board[x][y + 1] = " "; // for [9][1]
+        } else if (isBottomRow(x, y) && isLeftMostColoumn(x, y)) {
+          board[x][y] = Ship; // for [9][0]
+          board[x - 1][y] = " "; // for [8][0]
+          board[x][y + 1] = " "; // for [9][1]
+          board[x - 1][y + 1] = " "; // for [8][1]
+        } else if (isBottomRow(x, y) && isRightMostColumn(x, y)) {
+          board[x][y] = Ship; // for [9][9]
+          board[x - 1][y] = " "; // for [8][9]
+          board[x][y - 1] = " "; // for [9][8]
+          board[x - 1][y - 1] = " "; // for [8][8]
+        } else if (isTopRow(x, y)) {
+          board[x][y] = Ship; // taking [0][4] as an example
+          board[x][y - 1] = " "; // [0][3]
+          board[x][y + 1] = " "; // [0][5]
+          board[x + 1][y] = " "; // [1][4]
+          board[x + 1][y - 1] = " "; // [1][3]
+          board[x + 1][y + 1] = " "; // [1][5]
+        } else if (isBottomRow(x, y)) {
+          board[x][y] = Ship; //taking [9][4] as an example
+          board[x][y - 1] = " "; //[9][3]
+          board[x][y + 1] = " "; //[9][5]
+          board[x - 1][y] = " "; //[8][4]
+          board[x - 1][y - 1] = " "; //[8][3]
+          board[x - 1][y + 1] = " "; // [8][5]
+        } else if (isLeftMostColoumn(x, y)) {
+          board[x][y] = Ship; //[4][0]
+          board[x - 1][y] = " "; //[3][0]
+          board[x + 1][y] = " "; // [5][0]
+          board[x - 1][y + 1] = " "; //[3][1]
+          board[x][y + 1] = " "; //[4][1]
+          board[x + 1][y + 1] = " "; //[5][1]
+        } else if (isRightMostColumn(x, y)) {
+          board[x][y] = Ship; //[4][9]
+          board[x - 1][y] = " "; //[3][9]
+          board[x + 1][y] = " "; //[5][9]
+          board[x - 1][y - 1] = " "; // [3][8]
+          board[x][y - 1] = " "; // [4][8]
+          board[x + 1][y - 1] = ""; // [5][8]
+        } else {
+          board[x][y] = Ship; //[5][5]
+          board[x - 1][y - 1] = " "; //[4][4]
+          board[x - 1][y] = " "; //[4][5]
+          board[x - 1][y + 1] = " "; //[4][6]
+          board[x][y - 1] = " "; //[5][4]
+          board[x][y + 1] = " "; //[5][6]
+          board[x + 1][y - 1] = " "; //[6][4]
+          board[x + 1][y] = " "; //[6][5]
+          board[x + 1][y + 1] = " "; //[6][6]
+        }
       }
-    }
-  }
-}
-
-export {ship,gameBoard}
+    },
+  };
+};
+export { ship, gameBoard };
--- src/test.spec.js
+++ src/test.spec.js
@@ -1,37 +1,202 @@
 import { ship } from "./gameElements.js";
 import { gameBoard } from "./gameElements.js";
 
-test("test for ship isSunk", () => {
-  const mock = ship(1);
-  mock.hit();
-  expect(mock.isSunk()).toBe(true);
+describe("ship()", () => {
+  test("test for ship isSunk", () => {
+    const mock = ship(1);
+    mock.hit();
+    expect(mock.isSunk()).toBe(true);
+  });
+
+  test("test for ship hitCount", () => {
+    const mock = ship();
+    mock.hit();
+    mock.hit();
+    expect(mock.hitCount).toBe(2);
+  });
+
+  test("test for if ship is not sunk", () => {
+    const mock = ship(2);
+    mock.hit();
+    expect(mock.isSunk()).toBe(false);
+  });
+
+  test("test for ship's length", () => {
+    const mock = ship(6);
+    expect(mock.length).toBe(null);
+  });
+
+  test("test for ship's length", () => {
+    const mock = ship();
+    expect(mock.length).toBe(1);
+  });
+
+  test("test for ship's length", () => {
+    const mock = ship(2);
+    expect(mock.length).toBe(2);
+  });
 });
 
-test("test for ship hitCount", () => {
-  const mock = ship();
-  mock.hit();
-  mock.hit();
-  expect(mock.hitCount).toBe(2);
+describe("gameboard methods", () => {
+  describe("place horizontally method for ship", () => {
+    it("is placed on top row and leftmost column for ship with size 1", () => {
+      const board = gameBoard();
+      board.placeSingle(0, 0, (ship = ship()));
+      expect(board.board).toEqual([
+        [ship, " ", 2, 3, 4, 5, 6, 7, 8, 9],
+        [" ", " ", 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+    it("is place on top row and rightmost column for ship with size 1", () => {
+      const board = gameBoard();
+      board.placeSingle(9, 0);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", ship],
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", " "],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+    it("is placed on bottom row and leftmost column for ship with size 1", () => {
+      const board = gameBoard();
+      board.placeSingle(0, 9);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [" ", " ", 2, 3, 4, 5, 6, 7, 8, 9],
+        [ship, " ", 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+    it("is placed on bottom row and rightmost column for ship with size 1", () => {
+      const board = gameBoard();
+      board.placeSingle(9, 9);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", " "],
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", ship],
+      ]);
+    });
+    it("is placed on top row", () => {
+      const board = gameBoard();
+      board.placeSingle(0, 4);
+      expect(board.board).toEqual([
+        [0, 1, 2, " ", ship, " ", 6, 7, 8, 9],
+        [0, 1, 2, " ", " ", " ", 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+    it("is placed on bottom row", () => {
+      const board = gameBoard();
+      board.placeSingle(9, 4);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, " ", " ", " ", 6, 7, 8, 9],
+        [0, 1, 2, " ", ship, " ", 6, 7, 8, 9],
+      ]);
+    });
+    it("is placed on leftmost column", () => {
+      const board = gameBoard();
+      board.placeSingle(4, 0);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [" ", " ", 2, 3, 4, 5, 6, 7, 8, 9],
+        [ship, " ", 2, 3, 4, 5, 6, 7, 8, 9],
+        [" ", " ", 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+    it("is placed on rightmost column", () => {
+      const board = gameBoard();
+      board.placeSingle(4, 9);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", " "],
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", ship],
+        [0, 1, 2, 3, 4, 5, 6, 7, " ", " "],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+    it("is placed generally for ship with size 1", () => {
+      const board = gameBoard();
+      board.placeSingle(5, 5);
+      expect(board.board).toEqual([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, " ", " ", " ", 7, 8, 9],
+        [0, 1, 2, 3, " ", ship, " ", 7, 8, 9],
+        [0, 1, 2, 3, " ", " ", " ", 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ]);
+    });
+  });
+  it("board", () => {
+    const board = gameBoard();
+    expect(board.board).toEqual([
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ]);
+  });
 });
-
-test("test for if ship is not sunk", () => {
-  const mock = ship(2);
-  mock.hit();
-  expect(mock.isSunk()).toBe(false);
-});
-
-test("test for ship's length", () => {
-  const mock = ship(6);
-  expect(mock.length).toBe(null);
-});
-
-test("test for ship's length", () => {
-  const mock = ship();
-  expect(mock.length).toBe(1);
-});
-
-test("test for ship's length", () => {
-  const mock = ship(2);
-  expect(mock.length).toBe(2);
-});
-
