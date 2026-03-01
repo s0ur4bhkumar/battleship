d36ce424fb8aff1690234dc49556aefccaa26ec9
s0ur4bhkumar <soul65917@gmail.com>
2026-03-01 14:53:30 +0530

    add DOM helper functions markPlayerShip and cellFill

--- src/DOM.js
+++ src/DOM.js
@@ -1,15 +1,34 @@
 import { Ship, gameBoard } from "./gameElements.js";
 
+// ************************************************DOM HELPER FUNCTION*********************************************
+function cellFill(player, cell){
+  if (player !== ' ' && !Number.isInteger(player)) {
+    cell.ship = player
+  }
+}
+
+function markPlayerShip(cell) {
+  if (cell.ship) {
+    cell.style.backgroundColor = 'blue'
+  }
+}
+
+// ************************************************DOM MANIPULATION************************************************
+const player = gameBoard();
+const computer = gameBoard();
 const container = document.querySelector(".container");
 const playerBoard = document.createElement("div");
 const computerBoard = document.createElement("div");
 const randmBtn = document.createElement("button");
 const playerContainer = document.createElement("div");
 const computerContainer = document.createElement("div");
+player.randomPlaceShip();
+computer.randomPlaceShip();
 randmBtn.classList.add("randmBtn");
+randmBtn.textContent = "Randomize";
 playerContainer.classList.add("playerContainer");
 computerContainer.classList.add("computerContainer");
-playerContainer.append(playerBoard);
+playerContainer.append(playerBoard, randmBtn);
 computerContainer.append(computerBoard);
 playerBoard.classList.add("playerBoard");
 computerBoard.classList.add("computerBoard");
@@ -24,10 +43,13 @@ for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
     const playerCell = document.createElement("div");
     const computerCell = playerCell.cloneNode();
-    computerCell.classList.add("cell");
-    playerCell.classList.add("cell");
+    computerCell.classList.add("cell", "computerCell");
+    playerCell.classList.add("cell", "playerCell");
     playerCell.id = `playerCell${j}`;
     computerCell.id = `computerCell${j}`;
+    cellFill(player.board[i][j], playerCell)
+    cellFill(computer.board[i][j], computerCell)
+    markPlayerShip(playerCell)
     playerRow.append(playerCell);
     computerRow.append(computerCell);
   }
--- src/gameElements.js
+++ src/gameElements.js
@@ -250,6 +250,19 @@ const gameBoard = () => {
       }
       if (count === 10) {
         return true;
+      } else if (count > 10) {
+        return (board = [
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        ]);
       }
       return false;
     },
@@ -273,4 +286,7 @@ const gameBoard = () => {
     },
   };
 };
+// const board = gameBoard();
+// board.randomPlaceShip();
+// console.log(board.board);
 export { Ship, gameBoard };
