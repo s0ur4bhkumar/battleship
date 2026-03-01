5533d0b3b55819818654a2054b2d21684f2973fd
s0ur4bhkumar <soul65917@gmail.com>
2026-03-01 12:27:05 +0530

    modify computer and player grid attributes for better navigation and identification

--- src/DOM.js
+++ src/DOM.js
@@ -1,24 +1,35 @@
-import {Ship,gameBoard} from './gameElements.js'
+import { Ship, gameBoard } from "./gameElements.js";
 
 const container = document.querySelector(".container");
 const playerBoard = document.createElement("div");
 const computerBoard = document.createElement("div");
+const randmBtn = document.createElement("button");
+const playerContainer = document.createElement("div");
+const computerContainer = document.createElement("div");
+randmBtn.classList.add("randmBtn");
+playerContainer.classList.add("playerContainer");
+computerContainer.classList.add("computerContainer");
+playerContainer.append(playerBoard);
+computerContainer.append(computerBoard);
 playerBoard.classList.add("playerBoard");
 computerBoard.classList.add("computerBoard");
-container.append(playerBoard, computerBoard);
+container.append(playerContainer, computerContainer);
 
 for (let i = 0; i < 10; i++) {
   const playerRow = document.createElement("div");
   playerRow.classList.add("row");
   const computerRow = playerRow.cloneNode();
-  playerRow.id = `playerRow${i}`
-  computerRow.id = `computerRow${i}`
+  playerRow.id = `playerRow${i}`;
+  computerRow.id = `computerRow${i}`;
   for (let j = 0; j < 10; j++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.id = `cell${j}`;
-    playerRow.append(cell);
-    computerRow.append(cell.cloneNode())
+    const playerCell = document.createElement("div");
+    const computerCell = playerCell.cloneNode();
+    computerCell.classList.add("cell");
+    playerCell.classList.add("cell");
+    playerCell.id = `playerCell${j}`;
+    computerCell.id = `computerCell${j}`;
+    playerRow.append(playerCell);
+    computerRow.append(computerCell);
   }
   playerBoard.append(playerRow);
   computerBoard.append(computerRow);
